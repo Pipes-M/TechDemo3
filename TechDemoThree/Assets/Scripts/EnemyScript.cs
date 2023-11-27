b8501d515cc15f9c4ec8680754749037ac5ece40
MaxP <max.piper.uk@gmail.com>
2023-11-27 16:52:14 +0000

    ddddd

--- TechDemoThree/Assets/Scripts/EnemyScript.cs
+++ TechDemoThree/Assets/Scripts/EnemyScript.cs
@@ -1,18 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyScript : MonoBehaviour
 {
+    public bool isTarg;
+    private bool isPressed;
+    public GameObject targCirc;
+    private GameObject player;
+    private PlayerScript pScript;
+    public GameObject panel;
+    public Slider healhSlider;
+    public float health = 250f;
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
+        pScript = player.GetComponent<PlayerScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        if (pScript.pressing & !isPressed)
+        {
+            isTarg = false;
+            panel.SetActive(false);
+            targCirc.SetActive(false);
+            pScript.target = null;
+        }
+        healhSlider.value = health/250;
+        if (pScript.damage)
+        {
+            health -= 0.1f;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        isTarg = true;
+        isPressed = true;
+        pScript.target = gameObject;
+        panel.SetActive(true);
+        targCirc.SetActive(true);
+        pScript.target = gameObject;
+    }
+
+    private void OnMouseUp()
+    {
+        isPressed = false;
     }
 }
--- TechDemoThree/Assets/Scripts/PlayerScript.cs
+++ TechDemoThree/Assets/Scripts/PlayerScript.cs
@@ -1,18 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class PlayerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 5f;
+    private Rigidbody2D rigid;
+    private Vector2 movement;
+    public GameObject target;
+    public bool pressing;
+    public bool cantUnTarg;
+    public GameObject atckBut;
+    public bool damage;
+
     void Start()
     {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        // Get input from arrow keys
+        float horizontalInput = CrossPlatformInputManager.GetAxis("Horizontal");
+        float verticalInput = CrossPlatformInputManager.GetAxis("Vertical");
+
+        // Calculate movement direction
+        movement = new Vector2(horizontalInput, verticalInput);
+
+        // Normalize movement to ensure consistent speed in all directions
+
+        if(Input.GetMouseButtonDown(0) && !cantUnTarg)
+        {
+            pressing = true;
+            
+        }
+        else
+        {
+            pressing = false;
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        movement.Normalize();
+
+        // Move the player
+        rigid.MovePosition(rigid.position + movement * speed * Time.deltaTime);
+    }
+
+    private void OnMouseDown()
+    {
+        cantUnTarg = true;
+    }
+
+    private void OnMouseUp()
+    {
+        cantUnTarg = false;
+    }
+    public void autoAttack()
+    {
+        if (target != null)
+        {
+            damage = true;
+        }
+            
     }
 }
